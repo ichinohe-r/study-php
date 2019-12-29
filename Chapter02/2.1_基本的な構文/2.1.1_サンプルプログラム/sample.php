578509aecf82c072d8f68481cac1a6b4d474afb2
renichinohe <renichinohe@icloud.com>
2019-12-30 01:02:00 +0900

    終了タグの省略を確認

--- Chapter02/2.1_基本的な構文/2.1.1_サンプルプログラム/sample.php
+++ Chapter02/2.1_基本的な構文/2.1.1_サンプルプログラム/sample.php
@@ -1,10 +1,9 @@
 PHP スクリプトを実行します。
 <?php
 $number = rand();   // 乱数を取得します
-if ($number % 2 == 0 ) {
+if ($number % 2 == 0) {
     echo $number, "は偶数です。", PHP_EOL;
-}
-else {
+} else {
     echo $number, "は奇数です。", PHP_EOL;
 }
 ?>
